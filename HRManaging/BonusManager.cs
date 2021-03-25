5f81ce9a11b6d1f5745b465bba405cdb7d6ecc2b
Samara Gama <samaragamaferreirads@gmail.com>
2021-03-25 00:54:35 -0300

    Add BonusManager Class :sparkles:

---
+++ HRManaging/BonusManager.cs
@@ -0,0 +1,24 @@
+﻿using HRManaging.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManaging
+{
+    class BonusManager
+    {
+        private static double _totalBonus;
+        private static int _totalEmployees;
+        public void RegisterBonus(Employee employee)
+        {
+            _totalBonus += employee.GetBonus();
+            _totalEmployees++;
+        }
+        public double GetTotalBonus()
+        {
+            return _totalBonus;
+        }
+    }
+}
